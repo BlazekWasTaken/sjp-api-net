cfb149b187ff65d0be8268b2a7a6fab3f90b3cd2
Błażej Dymek <dymekblazej@outlook.com>
2023-08-13 18:50:54 +0200

    Added a Word model class with a constructor and access functions

--- sjp-api-net/Sjp.cs
+++ sjp-api-net/Sjp.cs
@@ -4,3 +4,28 @@ public static class Sjp
 {
     
 }
+
+public class Word
+{
+    private string Name { get; set; }
+    private bool IsAllowedInGames { get; set; }
+    private List<string> Definitions { get; set; }
+    public Word(string name, bool isAllowedInGames, List<string> definitions)
+    {
+        Name = name;
+        IsAllowedInGames = isAllowedInGames;
+        Definitions = definitions;
+    }
+    public string GetName()
+    {
+        return Name;
+    }
+    public bool GetIsAllowedInGames()
+    {
+        return IsAllowedInGames;
+    }
+    public List<string> GetDefinitions()
+    {
+        return Definitions;
+    }
+}
