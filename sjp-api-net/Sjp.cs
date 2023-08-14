b73d54056ac3febb03fd674478b7ca4f9649118b
Błażej Dymek <dymekblazej@outlook.com>
2023-08-14 18:47:51 +0200

    Added a function to get a Word object with all filled parameters

--- sjp-api-net/Sjp.cs
+++ sjp-api-net/Sjp.cs
@@ -1,8 +1,47 @@
-﻿namespace sjp_api_net;
+﻿using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+
+namespace sjp_api_net;
 
 public static class Sjp
 {
-    
+    public static Word GetWord(string word)
+    {
+        word = word.ToLower().Trim();
+        string url = $"https://sjp.pl/{word}";
+
+        string result;
+        try
+        {
+            HttpResponseMessage response = new HttpClient().GetAsync(url).Result;
+            HttpContent content = response.Content;
+            result = content.ReadAsStringAsync().Result;
+        }
+        catch
+        {
+            throw new Exception();
+        }
+        
+        if (result.Contains("nie występuje w słowniku")) throw new Exception();
+        
+        IHtmlDocument document = new HtmlParser().ParseDocument(result);
+
+        if (document.QuerySelectorAll("h1").All(x => x.InnerHtml.ToLower().Trim() != word)) 
+            throw new Exception();
+
+        string isAllowedHtml = document.QuerySelectorAll("p")
+            .FirstOrDefault(x => x.InnerHtml.Contains("dopuszczalne"))?
+            .InnerHtml ?? string.Empty;
+
+        if (!isAllowedHtml.Contains("dopuszczalne")) throw new Exception();
+
+        string isAllowed = isAllowedHtml.Split(" ").FirstOrDefault(x => x.Contains("dopuszczalne")) 
+                           ?? string.Empty;
+
+        List<string> definitions = document.QuerySelectorAll("p")[3].InnerHtml.Split("<br>").ToList();
+
+        return new Word(word, isAllowed == "dopuszczalne", definitions);
+    }
 }
 
 public class Word
